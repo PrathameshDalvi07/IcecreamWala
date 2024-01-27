4c07d6aad4a9657cff28cc661772ed1b780a37db
PrathameshDalvi07 <73353265+PrathameshDalvi07@users.noreply.github.com>
2024-01-27 16:22:52 +0530

    Added On Tap move Forward

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,30 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] GameObject roundObj;
+    [SerializeField] RectTransform roundRectTransform;
     [SerializeField] float offset = 5f;
     [SerializeField] float speed = 0.5f;
-    Vector2 roundPosition;
+    float inOutOfHolderSpeed = 2f;
+    [SerializeField] TextMeshProUGUI text;
+    public static UIManager Instance { get; private set; }
 
     void Start()
     {
-        roundPosition = roundObj.transform.localPosition;
+        Instance = this;
+        //SetRoundInOut();
+    }
+
+    public void SetTextAndPlayInOut(string txt)
+    {
+        text.text = txt;
         SetRoundInOut();
+    }
+
+    public void SetTextAndPlayIn(string txt)
+    {
+        text.text = txt;
+        roundRectTransform.DOAnchorPosY(0f, speed);
+    }
+
+    public void SetTextAndPlayOut()
+    {
+        roundRectTransform.DOAnchorPosY(100f, speed);
     }
 
     public void SetRoundInOut()
     {
-        //StartCoroutine(SetRoundInOutIEnum());
+        StartCoroutine(SetRoundInOutIEnum());
     }
 
     IEnumerator SetRoundInOutIEnum()
     {
-        roundObj.transform.DOLocalMoveY(roundPosition.x + offset, speed);
-        yield return new WaitForSeconds(speed);
-        //roundObj.transform.DOLocalMoveY(roundPosition.x - offset, speed);
+        roundRectTransform.DOAnchorPosY(0f, speed);
+        yield return new WaitForSeconds(inOutOfHolderSpeed);
+        roundRectTransform.DOAnchorPosY(100f, speed);
+    }
 
+    public void SetTimer(int timer)
+    {
+        StartCoroutine(SetTimerIEnum(timer));
+    }
+
+    IEnumerator SetTimerIEnum(int timer)
+    {
+        while (timer > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            text.text = timer.ToString();
+            --timer;
+        }
     }
 }
